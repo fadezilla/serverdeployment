f3f51d8606cb91a57bdd1124c727cf599aa8ccd6
fadezilla <harleyseierstad@hotmail.com>
2023-04-17 10:29:19 +0200

    test

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,13 @@ const add = require("../add");
 
 const router = new Router();
 
+router.get("/", (req, res) => {
+  res.json({
+    status: "success",
+    result: "Hello world"
+  })
+})
+
 router.get("/sum/:number1/:number2", (req, res) => {
   const {number1, number2} = req.params;
   if(number1 == null || number2 == null) {

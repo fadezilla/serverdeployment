1811f88a4e056ec5b1fc0b02f1a2d38682fd4621
fadezilla <harleyseierstad@hotmail.com>
2023-04-24 11:58:23 +0200

    fix

--- save_json.js
+++ save_json.js
@@ -1,12 +1,12 @@
 const fs = require("fs");
 const path = require("path");
-const AWS = require('aws-sdk');
+const AWS = require("aws-sdk");
 const s3 = new AWS.S3()
 
 const save = async (favNumber) => {
   console.log("saving");
   await s3.putObject({
-    Body:JSON.stringify(favNumber, null, 2),
+    Body: JSON.stringify(favNumber, null, 2),
     Bucket: "cyclic-plain-sweatpants-bear-us-east-2",
     Key: "number.json",
   }).promise()
